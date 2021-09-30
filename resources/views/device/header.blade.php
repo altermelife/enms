215ada4be9997fddc117cc1972f2e0f0389574c0
Tef Imperial <67237190+altermelife@users.noreply.github.com>
2021-09-30 12:41:49 +0800

    Update header.blade.php

--- resources/views/device/header.blade.php
+++ resources/views/device/header.blade.php
@@ -20,6 +20,6 @@
         <br style="clear: both;"/>
     </div>
     <div>
-        <iframe src="http://{{ $device->hostname }}/cgi-bin/home" style="text-align: left; vertical-align:top; height:500px; width:850px;" frameborder="0" scrolling="no"></iframe><iframe src="http://{{ $device->hostname }}/cgi-bin/currentfault_info" style="vertical-align:top; height:280px; width:400px;" frameborder="0"></iframe>
+        <iframe src="http://{{ $device->hostname }}/cgi-bin/home" style="text-align: left; vertical-align:top; height:500px; width:800px;" frameborder="0" scrolling="no"></iframe><iframe src="http://{{ $device->hostname }}/cgi-bin/currentfault_info" style="vertical-align:top; height:280px; width:400px;" frameborder="0"></iframe>
     </div>
 </x-panel>
