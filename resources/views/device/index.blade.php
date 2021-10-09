3469f53965837207667c41aa1f062fe7e6672ae1
Tef Imperial <67237190+altermelife@users.noreply.github.com>
2021-10-10 01:40:01 +0800

    Update index.blade.php

--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -7,14 +7,6 @@
         @include('device.header')
 
         <ul class="nav nav-tabs">
-            @foreach($tabs as $tab)
-                @if($tab->visible($device))
-                    <li role="presentation" @if( $current_tab == $tab->slug() ) class="active" @endif>
-                        <a href="{{ route('device', [$device_id, $tab->slug()]) }}">
-                            <i class="fa {{ $tab->icon() }} fa-lg icon-theme" aria-hidden="true"></i>&nbsp;{{ $tab->name() }}&nbsp;</a>
-                    </li>
-                @endif
-            @endforeach
 
             <div class="btn-group pull-right" role="group">
                 <a href="{{ $primary_device_link['url'] }}"
