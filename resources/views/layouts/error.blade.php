a4fc1c0f3ee5ac65dba357764406c2648d15ea9b
Tef Imperial <67237190+altermelife@users.noreply.github.com>
2021-11-01 20:48:01 +0800

    support contact change

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -74,7 +74,7 @@
     <hr class="separator"/>
     <p>@lang("Check your log for more details.") ({{ isset($log_file) ? $log_file : 'librenms.log' }})</p>
 
-    <p>@lang("If you need additional help, you can find how to get help at") <a target="_blank" href="https://docs.librenms.org/Support">https://docs.librenms.org/Support</a>.</p>
+    <p>@lang("If you need additional help, you can find how to get help at") <a target="_blank" href="https://support.ionlotus.com">https://support.ionlotus.com</a>.</p>
 </div>
 </body>
 </html>
